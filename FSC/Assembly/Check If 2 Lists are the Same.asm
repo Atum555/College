de90f76bae8dcd2b85d1a40f3bba5d65f242ad85
Cláudio <claudiomeireles555@gmail.com>
2023-11-14 12:11:31 +0000

    Update

--- FSC/Assembly/Check If 2 Lists are the Same.asm
+++ FSC/Assembly/Check If 2 Lists are the Same.asm
@@ -29,22 +29,22 @@ D2:
 93
 
 .text
-la x28, D1 		# Load adress of list 1 to x28
-la x29, D2  		# Load adress of list 2 to x29
-li x10, 1		# Init result to 1
-li x5, 10		# Init counter to 1
+la x28, D1 	        # Load adress of list 1 to x28
+la x29, D2      	# Load adress of list 2 to x29
+li x10, 1	        # Init result to 1
+li x5, 10	        # Init counter to 1
 
 L1:
-lw x30, (x28)		# Load values from list 1
-lw x31, (x29)		# Load values from list 2
-bne x30,x31,L2		# Branch to L2 if not equal
-addi x28, x28, 4 		# Add 4 to adsress of list 1
-addi x29, x29, 4		# Add 4 to address of list 2
-addi x5, x5, -1		# Incress counter
-beqz x5, L3		# Jump to end if counter is done
+lw x30, (x28)       # Load values from list 1
+lw x31, (x29)       # Load values from list 2
+bne x30,x31,L2      # Branch to L2 if not equal
+addi x28, x28, 4    # Add 4 to adsress of list 1
+addi x29, x29, 4    # Add 4 to address of list 2
+addi x5, x5, -1	    # Incress counter
+beqz x5, L3	        # Jump to end if counter is done
 b L1
 
 L2:
-li x10, 0 		# Load result if not Equal
+li x10, 0           # Load result if not Equal
 
 L3:
--- FSC/Assembly/Count NonZero Values.asm
+++ FSC/Assembly/Count NonZero Values.asm
@@ -8,15 +8,15 @@ D1:
 87
 
 .text
-la x29, D1 		# Load adress to x29
-li x28, 0  		# Contador 0
+la x29, D1      # Load adress to x29
+li x28, 0       # Contador 0
 
 L1:
-lw x30,(x29) 		# Load next to x30
-beqz x30, L2 		# Zero found, jump end
-addi x28, x28, 1 	# Add one to conter
-addi x29, x29, 4 	# Add 4 to adrress
+lw x30,(x29)        # Load next to x30
+beqz x30, L2        # Zero found, jump end
+addi x28, x28, 1    # Add one to conter
+addi x29, x29, 4    # Add 4 to adrress
 b L1
 
 L2:
-add x5,x28,x0		# Load result to x5
+add x5,x28,x0       # Load result to x5
