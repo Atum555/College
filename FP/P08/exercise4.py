55a85df32c97956c060f99487e0617c044de3785
Cláudio <claudiomeireles555@gmail.com>
2023-11-15 09:36:13 +0000

    Update

--- FP/P08/exercise4.py
+++ FP/P08/exercise4.py
@@ -1,7 +1,11 @@
-def permutations(text:str)-> list:
-    if len(text) <2 : return [text]
+def permutations(string:str)-> list:
+    if len(string) <2 : return [string]
+
     result = []
-    for i,l in enumerate(text):
-        for x in permutations(text[:i]+text[i+1:]):
-            result.append(l+x)
+    for i,letter in enumerate(string):                          # Fazer um Loop por todos os caracteres 
+        string_without_l = string[:i]+string[i+1:]              # String without a letra do loop
+        permut_of_sub_string = permutations(string_without_l)   # Lista de permutações da sub String (String exceto a lettra atual do loop)
+        for permutation in permut_of_sub_string:
+            result.append(letter+permutation)       # Adicionar "letra"+"resultado" à lista dos resultados
+
     return result
