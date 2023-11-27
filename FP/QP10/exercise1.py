4c9d49fe9bb28f671e36b9cc2f58c53dd6e0e1d5
Cláudio <claudiomeireles555@gmail.com>
2023-11-27 14:18:14 +0000

    Update

--- FP/QP10/exercise1.py
+++ FP/QP10/exercise1.py
@@ -1,1 +1,17 @@
+# One-liner
 def square_odds(values:str): return ",".join(map(str, map(lambda x: int(x)**2, filter(lambda x: int(x) % 2 == 1, values.split(",")))))
+
+# Normal
+def square_odds(values:str):
+    valuesList = values.split(",")          # Split values
+    valuesList = map(int, valuesList)       # Convert to int
+
+    isOdd = lambda x: x % 2 == 1            # Function to check if number is odd
+    valuesList = filter(isOdd, valuesList)  # Select only odd numbers
+
+    square = lambda x: x**2                 # Function that returns the square of a given number
+    valuesList = map(square, valuesList)    # Compute squares
+
+    valuesList = map(str, valuesList)       # Convert int to string
+    result = ",".join(valuesList)           # Join string seperated by a comma
+    return result
--- FP/QP10/exercise2.py
+++ FP/QP10/exercise2.py
@@ -1,2 +1,23 @@
+# One-liner
 import math
 def comprehensions(i:int, j:int):  return list(filter(lambda x: x % 10 == 3 or x % 10 == 8, range(i,j+1))), tuple(map(lambda x: round(math.sqrt(x), 2), range(i,j+1))), dict(map(lambda x: (x, chr(x)), range(i,j+1)))
+
+# Normal
+import math
+def comprehensions(i:int, j:int):
+    interval = range(i,j+1)
+
+    is3or8 = lambda x: x % 10 == 3 or x % 10 == 8
+    theList = filter(is3or8, interval)
+    theList = list(theList)
+
+    def sqr(n:int):
+        value = math.sqrt(n)
+        result = round(value, 2)
+        return result
+    theTuple = map(sqr, interval)
+    theTuple = tuple(theTuple)
+
+    theDict = {x: chr(x) for x in interval}
+
+    return (theList, theTuple, theDict)
