a6f4500ca924d05e5984c301024f5d5af9987522
Cláudio <claudiomeireles555@gmail.com>
2023-11-27 14:47:42 +0000

    Update

--- FP/QP10/exercise3.py
+++ FP/QP10/exercise3.py
@@ -1,1 +1,8 @@
-def pythagoreans(a:int, b:int): return list((x,y,z) for x in range(a,b) for y in range(a,b) for z in range(a,b) if x **2 + y **2 == z **2 and x < y < z)
+# One-liner
+def pythagoreans(a:int, b:int): return list((x,y,z) for x in range(a,b) for y in range(a,b) for z in range(a,b) if x**2 + y**2 == z**2 and x < y < z)
+
+# Normal
+def pythagoreans(a:int, b:int):
+    r = range(a,b)
+    values = ((x,y,z) for x in r   for y in range(x,b)   for z in r    if x**2 + y**2 == z**2 )
+    return list(values)
--- FP/QP10/exercise4.py
+++ FP/QP10/exercise4.py
@@ -1,1 +1,19 @@
+# One-liner
 def union_with(combine, dict1:dict, dict2:dict): return {**{x: combine(dict1[x], dict2[x]) for x in dict1 if dict2.get(x) != None}, **{x: dict1[x] for x in dict1 if x not in dict2}, **{x:dict2[x] for x in dict2 if x not in dict1}}
+
+# Normal
+def union_with(combine, dict1:dict, dict2:dict):
+    def handle(key):
+        if key in dict1 and key in dict2:
+            value1 = dict1[key]
+            value2 = dict2[key]
+            result = combine(value1, value2)
+            return result
+        elif key in dict1:
+            return dict1[key]
+        elif key in dict2:
+            return dict2[key]
+        
+    keys = dict1.keys() | dict2.keys()          # Conjunto de todas as chaves
+    result = {key: handle(key) for key in keys} 
+    return result
