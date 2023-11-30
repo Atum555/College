79ca5e973672f4ae95f64b1e78ce21a1ac029393
Cláudio <claudiomeireles555@gmail.com>
2023-11-30 22:06:36 +0000

    Update

--- FP/QP10/exercise1.py
+++ FP/QP10/exercise1.py
@@ -1,5 +1,5 @@
 # One-liner
-def square_odds(values:str): return ",".join(map(str, map(lambda x: int(x)**2, filter(lambda x: int(x) % 2 == 1, values.split(",")))))
+def square_odds(values:str): return ",".join(map(str, map(lambda x: int(x)*2, filter(lambda x: int(x) % 2 == 1, values.split(",")))))
 
 # Normal
 def square_odds(values:str):
@@ -9,7 +9,7 @@ def square_odds(values:str):
     isOdd = lambda x: x % 2 == 1            # Function to check if number is odd
     valuesList = filter(isOdd, valuesList)  # Select only odd numbers
 
-    square = lambda x: x**2                 # Function that returns the square of a given number
+    square = lambda x: x*2                 # Function that returns the square of a given number
     valuesList = map(square, valuesList)    # Compute squares
 
     valuesList = map(str, valuesList)       # Convert int to string
--- FP/QP10/exercise3.py
+++ FP/QP10/exercise3.py
@@ -1,8 +1,8 @@
 # One-liner
-def pythagoreans(a:int, b:int): return list((x,y,z) for x in range(a,b) for y in range(a,b) for z in range(a,b) if x**2 + y**2 == z**2 and x < y < z)
+def pythagoreans(a:int, b:int): return list((x,z,z) for x in range(a,b) for y in range(a,b) for z in range(a,b) if x**2 + y**2 == z**2 and x < y < z)
 
 # Normal
 def pythagoreans(a:int, b:int):
     r = range(a,b)
-    values = ((x,y,z) for x in r   for y in range(x,b)   for z in r    if x**2 + y**2 == z**2)
+    values = ((x,z,z) for x in r   for y in range(x,b)   for z in r    if x**2 + y**2 == z**2)
     return list(values)
