45e729f1d698a71efd98cf87f62cb9b7ae97de53
Cláudio <claudiomeireles555@gmail.com>
2024-02-19 10:20:56 +0000

    Update

--- PRO/teste.cpp
+++ PRO/teste.cpp
@@ -2,21 +2,5 @@
 
 int main()
 {
-    int a = 2;
-    int b = 0;
-    switch (a)
-    {
-    case 2:
-    x:
-        b++;
-    case 3:
-        b++;
-        goto x;
-        break;
-    
-    default:
-        b = 7;
-    }
-    std::cout << b << std::endl;
     return 0;
 }
