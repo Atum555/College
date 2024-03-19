cd0004908ec831da6d5871ec6be71a7da0a3afdc
Cláudio Meireles <claudiomeireles555@gmail.com>
2024-03-19 14:56:56 +0000

    Update

--- PRO/teste.cpp
+++ PRO/teste.cpp
@@ -1,9 +1,40 @@
+#include <algorithm>
 #include <cstring>
 #include <iostream>
 #include <string>
+#include <unistd.h>
+
+void loadText(const char *str) {
+    char letters[]
+        = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    int i = 0;
+    while (char l = str[i]) {
+        if (!(('a' <= l and l <= 'z') or ('A' <= l and l <= 'Z')
+              or ('0' <= l and l <= '9'))) {
+            ++i;
+            continue;
+        }
+        int j = 0;
+        while (letters[j] != l) {
+            usleep(5000);
+            std::cout << "\r";
+            for (int k = 0; k < i; ++k) std::cout << str[k];
+            std::cout << letters[j];
+            std::flush(std::cout);
+            ++j;
+        }
+        usleep(5000);
+        std::cout << "\r";
+        for (int k = 0; k <= i; ++k) std::cout << str[k];
+        std::flush(std::cout);
+        ++i;
+    }
+    std::cout << "\r";
+    std::cout << str << std::endl;
+}
 
 int main() {
-    int a[] = { 1, 44, 65 };
-    std::cout << 0 [a] << 1 [a] << 2 [a] << std::endl;
+    loadText("A minha pila é muito grande!");
+    std::string atum = "432";
     return 0;
 }
