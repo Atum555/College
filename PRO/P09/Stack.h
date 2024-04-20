522e852c5f6f96398207a9eeb3c031a6c6e9b5f2
Cláudio Meireles <claudiomeireles555@gmail.com>
2024-04-20 18:34:37 +0100

    Add Exercise 5 of P09_PRO

---
+++ PRO/P09/Stack.h
@@ -0,0 +1,20 @@
+#ifndef STACK_H
+#define STACK_H
+
+#include <iomanip>
+#include <iostream>
+#include <vector>
+
+template <typename T> class Stack {
+  public:
+    Stack();
+    size_t size() const;
+    bool   peek(T &elem) const;
+    bool   pop(T &elem);
+    void   push(const T &elem);
+
+  private:
+    std::vector<T> elements;
+};
+
+#endif
