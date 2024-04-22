b087b942b7f7a9107ccc3c5faac0d6c5bc144414
Atum555 <claudiomeireles555@gmail.com>
2024-04-22 14:05:37 +0100

    Add Exercise 1 of P10_PRO

---
+++ PRO/P10/Product.h
@@ -0,0 +1,17 @@
+#ifndef __Product_h__
+#define __Product_h__
+
+class Product {
+  public:
+    Product(int id, float price) : id_(id), price_(price){};
+
+    int get_id() const { return id_; }
+
+    virtual float get_sell_price() const { return price_; }
+
+  protected:
+    int   id_;
+    float price_;
+};
+
+#endif
