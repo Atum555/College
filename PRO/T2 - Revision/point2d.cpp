31babcbaca78e4b452e574558ef827190ad20970
Atum555 <claudiomeireles555@gmail.com>
2024-05-22 17:14:53 +0100

    Add Exercise 12 of T2 - Revision

---
+++ PRO/T2 - Revision/point2d.cpp
@@ -0,0 +1,9 @@
+#include "Point2d.h"
+#include <cmath>
+
+void Point2d::translate(const Point2d &t) {
+    x += t.x;
+    y += t.y;
+}
+
+double Point2d::distance_to(const Point2d &p) const { return sqrt(pow(x - p.x, 2) + pow(y - p.y, 2)); }
