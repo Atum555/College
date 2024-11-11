ca7a9a944c3c737e4ad775b2c58666fac86f6d91
Claudio Meireles <claudiomeireles555@gmail.com>
2024-11-11 11:59:52 +0000

    WIP

---
+++ AED/Practical Class 06/031 - Golden Permits.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+int main() {
+    u_int64_t      N;
+    u_int64_t      K;
+    u_int64_t      W;
+    list<u_int8_t> numbers;
+
+    // Read Data
+    cin >> N >> K >> W;
+
+    for (size_t i = 1; i <= N; i++) {}
+}
---
+++ AED/Practical Class 06/1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <vector>
+
+void jump2(u_int64_t )
+
+int main() {
+
+}
