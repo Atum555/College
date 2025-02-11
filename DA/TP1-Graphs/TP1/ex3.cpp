a4fd7363111fef449cc72a264f2068704dc5901d
Claudio Meireles <claudiomeireles555@gmail.com>
2025-02-11 15:10:20 +0000

    Add Exercise 3 of DA/TP1-Graphs

--- DA/TP1-Graphs/TP1/ex3.cpp
+++ DA/TP1-Graphs/TP1/ex3.cpp
@@ -1,5 +1,5 @@
-#include <iostream>
 #include "../data_structures/Graph.h"
+#include <iostream>
 
 using namespace std;
 
@@ -11,10 +11,20 @@ using namespace std;
  * that is being processed in the stack of recursive calls.
  * Returns true if the graph is acyclic, and false otherwise.
  */
-
 template <class T>
 bool isDAG(Graph<T> *g) {
-    // TODO
+    // Mark all vertices as unvisited
+    for (Vertex<T> *v : g->getVertexSet()) v->setVisited(false);
+
+    // Mark all vertices as not processing
+    for (Vertex<T> *v : g->getVertexSet()) v->setProcessing(false);
+
+    // For all non visited nodes, performe a DFS
+    for (Vertex<T> *v : g->getVertexSet())
+        if (not v->isVisited())
+            // If cycle found, stop and rerturn empty list
+            if (not dfsIsDAG_aux(v)) return false;
+
     return true;
 }
 
@@ -23,8 +33,27 @@ bool isDAG(Graph<T> *g) {
  * Returns false (not acyclic) if an edge to a vertex in the stack is found.
  */
 template <class T>
-bool dfsIsDAG(Vertex<T> *v) {
-    // TODO
+bool dfsIsDAG_aux(Vertex<T> *v) {
+    // Mark vertex as processing
+    v->setProcessing(true);
+
+    // For each connected vertex
+    for (Edge<T> *edge : v->getAdj()) {
+        Vertex<T> *child = edge->getDest();
+
+        // If vertex already visited ignore
+        if (child->isVisited()) continue;
+
+        // If vertex processing, cycle found, abort
+        if (child->isProcessing()) return false;
+
+        // Process child
+        if (not dfsIsDAG_aux(child)) return false;
+    }
+
+    // Mark vertex as visited
+    v->setVisited(true);
+
+    // No cycles found
     return true;
 }
-
